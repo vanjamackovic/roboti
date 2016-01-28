7409ada206da9e8bd1b1068a3020a2f65cce9bb2
vanjamackovic <vanja.mackovic@skole.hr>
2016-01-28 13:40:46 +0100

    drugi commit

--- robot1.py
+++ robot1.py
@@ -1,3 +1,4 @@
+i = 0
 while(i<11):
-    print("Ja sam robot Luka")
+    print("Ja sam robot Luka!")
     i = i + 1
--- robot2.py
+++ robot2.py
@@ -1,3 +1,4 @@
+i = 0
 while(i<11):
-    print("Ja sam robot Crnjo")
-    i = i+1
+    print("Ja sam robot Crnjo!")
+    i = i + 1
